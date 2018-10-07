4d26b7c9b558d092fc73cb699f9012984bb4b985
Rohan Varma <rvarm1@gmail.com>
2018-10-07 16:08:38 -0700

    fix reuse isues by removing assert and setting reuse=True when doing image sampling

--- gan_train.py
+++ gan_train.py
@@ -96,32 +96,33 @@ def avg_pool_2x2(x):
 # In[11]:
 
 def discriminator(x_image, reuse=False):
-    if (reuse):
-        tf.get_variable_scope().reuse_variables()
-    #First Conv and Pool Layers
-    W_conv1 = tf.get_variable('d_wconv1', [5, 5, 1, 8], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    b_conv1 = tf.get_variable('d_bconv1', [8], initializer=tf.constant_initializer(0))
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = avg_pool_2x2(h_conv1)
+    with tf.variable_scope('discriminator') as scope:
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        #First Conv and Pool Layers
+        W_conv1 = tf.get_variable('d_wconv1', [5, 5, 1, 8], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_conv1 = tf.get_variable('d_bconv1', [8], initializer=tf.constant_initializer(0))
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+        h_pool1 = avg_pool_2x2(h_conv1)
 
-    #Second Conv and Pool Layers
-    W_conv2 = tf.get_variable('d_wconv2', [5, 5, 8, 16], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    b_conv2 = tf.get_variable('d_bconv2', [16], initializer=tf.constant_initializer(0))
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = avg_pool_2x2(h_conv2)
+        #Second Conv and Pool Layers
+        W_conv2 = tf.get_variable('d_wconv2', [5, 5, 8, 16], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_conv2 = tf.get_variable('d_bconv2', [16], initializer=tf.constant_initializer(0))
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = avg_pool_2x2(h_conv2)
 
-    #First Fully Connected Layer
-    W_fc1 = tf.get_variable('d_wfc1', [7 * 7 * 16, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    b_fc1 = tf.get_variable('d_bfc1', [32], initializer=tf.constant_initializer(0))
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        #First Fully Connected Layer
+        W_fc1 = tf.get_variable('d_wfc1', [7 * 7 * 16, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_fc1 = tf.get_variable('d_bfc1', [32], initializer=tf.constant_initializer(0))
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*16])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    #Second Fully Connected Layer
-    W_fc2 = tf.get_variable('d_wfc2', [32, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    b_fc2 = tf.get_variable('d_bfc2', [1], initializer=tf.constant_initializer(0))
+        #Second Fully Connected Layer
+        W_fc2 = tf.get_variable('d_wfc2', [32, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b_fc2 = tf.get_variable('d_bfc2', [1], initializer=tf.constant_initializer(0))
 
-    #Final Layer
-    y_conv=(tf.matmul(h_fc1, W_fc2) + b_fc2)
+        #Final Layer
+        y_conv=(tf.matmul(h_fc1, W_fc2) + b_fc2)
     return y_conv
 
 
@@ -136,60 +137,61 @@ def discriminator(x_image, reuse=False):
 # In[1]:
 
 def generator(z, batch_size, z_dim, reuse=False):
-    if (reuse):
-        tf.get_variable_scope().reuse_variables()
-    g_dim = 64 #Number of filters of first layer of generator 
-    c_dim = 1 #Color dimension of output (MNIST is grayscale, so c_dim = 1 for us)
-    s = 28 #Output size of the image
-    s2, s4, s8, s16 = int(s/2), int(s/4), int(s/8), int(s/16) #We want to slowly upscale the image, so these values will help
-                                                              #make that change gradual.
+    with tf.variable_scope('generator') as scope:
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        g_dim = 64 #Number of filters of first layer of generator 
+        c_dim = 1 #Color dimension of output (MNIST is grayscale, so c_dim = 1 for us)
+        s = 28 #Output size of the image
+        s2, s4, s8, s16 = int(s/2), int(s/4), int(s/8), int(s/16) #We want to slowly upscale the image, so these values will help
+                                                                  #make that change gradual.
 
-    h0 = tf.reshape(z, [batch_size, s16+1, s16+1, 25])
-    h0 = tf.nn.relu(h0)
-    #Dimensions of h0 = batch_size x 2 x 2 x 25
+        h0 = tf.reshape(z, [batch_size, s16+1, s16+1, 25])
+        h0 = tf.nn.relu(h0)
+        #Dimensions of h0 = batch_size x 2 x 2 x 25
 
-    #First DeConv Layer
-    output1_shape = [batch_size, s8, s8, g_dim*4]
-    W_conv1 = tf.get_variable('g_wconv1', [5, 5, output1_shape[-1], int(h0.get_shape()[-1])], 
-                              initializer=tf.truncated_normal_initializer(stddev=0.1))
-    b_conv1 = tf.get_variable('g_bconv1', [output1_shape[-1]], initializer=tf.constant_initializer(.1))
-    H_conv1 = tf.nn.conv2d_transpose(h0, W_conv1, output_shape=output1_shape, 
-                                     strides=[1, 2, 2, 1], padding='SAME') + b_conv1
-    H_conv1 = tf.contrib.layers.batch_norm(inputs = H_conv1, center=True, scale=True, is_training=True, scope="g_bn1")
-    H_conv1 = tf.nn.relu(H_conv1)
-    #Dimensions of H_conv1 = batch_size x 3 x 3 x 256
+        #First DeConv Layer
+        output1_shape = [batch_size, s8, s8, g_dim*4]
+        W_conv1 = tf.get_variable('g_wconv1', [5, 5, output1_shape[-1], int(h0.get_shape()[-1])], 
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b_conv1 = tf.get_variable('g_bconv1', [output1_shape[-1]], initializer=tf.constant_initializer(.1))
+        H_conv1 = tf.nn.conv2d_transpose(h0, W_conv1, output_shape=output1_shape, 
+                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv1
+        H_conv1 = tf.contrib.layers.batch_norm(inputs = H_conv1, center=True, scale=True, is_training=True, scope="g_bn1")
+        H_conv1 = tf.nn.relu(H_conv1)
+        #Dimensions of H_conv1 = batch_size x 3 x 3 x 256
 
-    #Second DeConv Layer
-    output2_shape = [batch_size, s4 - 1, s4 - 1, g_dim*2]
-    W_conv2 = tf.get_variable('g_wconv2', [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])], 
-                              initializer=tf.truncated_normal_initializer(stddev=0.1))
-    b_conv2 = tf.get_variable('g_bconv2', [output2_shape[-1]], initializer=tf.constant_initializer(.1))
-    H_conv2 = tf.nn.conv2d_transpose(H_conv1, W_conv2, output_shape=output2_shape, 
-                                     strides=[1, 2, 2, 1], padding='SAME') + b_conv2
-    H_conv2 = tf.contrib.layers.batch_norm(inputs = H_conv2, center=True, scale=True, is_training=True, scope="g_bn2")
-    H_conv2 = tf.nn.relu(H_conv2)
-    #Dimensions of H_conv2 = batch_size x 6 x 6 x 128
+        #Second DeConv Layer
+        output2_shape = [batch_size, s4 - 1, s4 - 1, g_dim*2]
+        W_conv2 = tf.get_variable('g_wconv2', [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])], 
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b_conv2 = tf.get_variable('g_bconv2', [output2_shape[-1]], initializer=tf.constant_initializer(.1))
+        H_conv2 = tf.nn.conv2d_transpose(H_conv1, W_conv2, output_shape=output2_shape, 
+                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv2
+        H_conv2 = tf.contrib.layers.batch_norm(inputs = H_conv2, center=True, scale=True, is_training=True, scope="g_bn2")
+        H_conv2 = tf.nn.relu(H_conv2)
+        #Dimensions of H_conv2 = batch_size x 6 x 6 x 128
 
-    #Third DeConv Layer
-    output3_shape = [batch_size, s2 - 2, s2 - 2, g_dim*1]
-    W_conv3 = tf.get_variable('g_wconv3', [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])], 
-                              initializer=tf.truncated_normal_initializer(stddev=0.1))
-    b_conv3 = tf.get_variable('g_bconv3', [output3_shape[-1]], initializer=tf.constant_initializer(.1))
-    H_conv3 = tf.nn.conv2d_transpose(H_conv2, W_conv3, output_shape=output3_shape, 
-                                     strides=[1, 2, 2, 1], padding='SAME') + b_conv3
-    H_conv3 = tf.contrib.layers.batch_norm(inputs = H_conv3, center=True, scale=True, is_training=True, scope="g_bn3")
-    H_conv3 = tf.nn.relu(H_conv3)
-    #Dimensions of H_conv3 = batch_size x 12 x 12 x 64
+        #Third DeConv Layer
+        output3_shape = [batch_size, s2 - 2, s2 - 2, g_dim*1]
+        W_conv3 = tf.get_variable('g_wconv3', [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])], 
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b_conv3 = tf.get_variable('g_bconv3', [output3_shape[-1]], initializer=tf.constant_initializer(.1))
+        H_conv3 = tf.nn.conv2d_transpose(H_conv2, W_conv3, output_shape=output3_shape, 
+                                         strides=[1, 2, 2, 1], padding='SAME') + b_conv3
+        H_conv3 = tf.contrib.layers.batch_norm(inputs = H_conv3, center=True, scale=True, is_training=True, scope="g_bn3")
+        H_conv3 = tf.nn.relu(H_conv3)
+        #Dimensions of H_conv3 = batch_size x 12 x 12 x 64
 
-    #Fourth DeConv Layer
-    output4_shape = [batch_size, s, s, c_dim]
-    W_conv4 = tf.get_variable('g_wconv4', [5, 5, output4_shape[-1], int(H_conv3.get_shape()[-1])], 
-                              initializer=tf.truncated_normal_initializer(stddev=0.1))
-    b_conv4 = tf.get_variable('g_bconv4', [output4_shape[-1]], initializer=tf.constant_initializer(.1))
-    H_conv4 = tf.nn.conv2d_transpose(H_conv3, W_conv4, output_shape=output4_shape, 
-                                     strides=[1, 2, 2, 1], padding='VALID') + b_conv4
-    H_conv4 = tf.nn.tanh(H_conv4)
-    #Dimensions of H_conv4 = batch_size x 28 x 28 x 1
+        #Fourth DeConv Layer
+        output4_shape = [batch_size, s, s, c_dim]
+        W_conv4 = tf.get_variable('g_wconv4', [5, 5, output4_shape[-1], int(H_conv3.get_shape()[-1])], 
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b_conv4 = tf.get_variable('g_bconv4', [output4_shape[-1]], initializer=tf.constant_initializer(.1))
+        H_conv4 = tf.nn.conv2d_transpose(H_conv3, W_conv4, output_shape=output4_shape, 
+                                         strides=[1, 2, 2, 1], padding='VALID') + b_conv4
+        H_conv4 = tf.nn.tanh(H_conv4)
+        #Dimensions of H_conv4 = batch_size x 28 x 28 x 1
 
     return H_conv4
 
@@ -284,13 +286,9 @@ g_vars = [var for var in tvars if 'g_' in var.name]
 
 # In[16]:
 
-with tf.variable_scope(tf.get_variable_scope(),reuse=False): 
-    print("reuse or not: {}".format(tf.get_variable_scope().reuse))
-    assert tf.get_variable_scope().reuse == False, "Houston tengo un problem"
-    trainerD = tf.train.AdamOptimizer().minimize(d_loss, var_list=d_vars)
-    trainerG = tf.train.AdamOptimizer().minimize(g_loss, var_list=g_vars)
-print("exiting")
-exit()
+adam = tf.train.AdamOptimizer()
+trainerD = adam.minimize(d_loss, var_list=d_vars)
+trainerG = adam.minimize(g_loss, var_list=g_vars)
 
 
 # You can also specify a learning rate by passing it as an argument (I’ve found .0002 to be effective). 
@@ -315,11 +313,11 @@ for i in range(iterations):
 
 # In[162]:
 
-# sample_image = generator(z_placeholder, 1, z_dimensions)
-# z_batch = np.random.normal(-1, 1, size=[1, z_dimensions])
-# temp = (sess.run(sample_image, feed_dict={z_placeholder: z_batch}))
-# my_i = temp.squeeze()
-# plt.imshow(my_i, cmap='gray_r')
+sample_image = generator(z_placeholder, 1, z_dimensions, reuse=True)
+z_batch = np.random.normal(-1, 1, size=[1, z_dimensions])
+temp = (sess.run(sample_image, feed_dict={z_placeholder: z_batch}))
+my_i = temp.squeeze()
+plt.imshow(my_i, cmap='gray_r')
 
 
 # # Training Difficulties
